69d329bf0da7304ddfcc7bba9a04154ba769b29d
Enoch Garoli <eenkiga@gmail.com>
2025-04-01 10:38:58 +0300

    TodoList

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,20 @@
+// src/components/TodoList.js
+import TodoItem from './TodoItem';
+
+const TodoList = ({ todos, setCurrentTodo, deleteTodo, toggleComplete }) => {
+  return (
+    <div className="todo-list">
+      {todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          setCurrentTodo={setCurrentTodo}
+          deleteTodo={deleteTodo}
+          toggleComplete={toggleComplete}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
